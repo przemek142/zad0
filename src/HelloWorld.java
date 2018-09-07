74445ec129973aa774f14bfec619a4e3aafa8dea
Przemysław Sitek <przemo2@gmail.com>
2018-09-07 19:25:31 +0200

    fghj

---
+++ src/HelloWorld.java
@@ -0,0 +1,13 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("hello");
+
+        System.out.print("word");
+        System.out.print("word2");
+    }
+
+
+
+}
+
+
